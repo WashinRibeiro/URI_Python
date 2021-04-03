dec744cc2870f1d478158a2e9302e0a602550b44
Washington Ribeiro da Silva <66757021+WashinRibeiro@users.noreply.github.com>
2021-04-03 19:29:56 -0300

    +questões

---
+++ URI em Python/URI 1259 - Pares e Ímpares.py
@@ -0,0 +1,19 @@
+qtd = int(input())
+
+pares = []
+impares = []
+for n in range(qtd):
+    num = int(input())
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+
+pares.sort()
+impares.sort(reverse=True)
+
+for p in pares:
+    print(p)
+
+for i in impares:
+    print(i)
